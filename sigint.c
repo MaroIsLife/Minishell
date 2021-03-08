fad4319b5bff807d2d63af5f8d18958fa3ae95ce
Marouane Ougnou <mougnou@e1r12p6.1337.ma>
2021-03-08 18:15:05 +0100

    Fixing Minor bugs in Parsing

---
+++ sigint.c
@@ -0,0 +1,24 @@
+// #include <signal.h>
+// #include <unistd.h>
+// #include <stdio.h>
+
+// void no(int c)
+// {
+//     write(1,"\nNo\n",4);
+// }
+
+// int main()
+// {
+
+//     signal(SIGINT,no); //C
+//     signal(SIGQUIT,no); //D
+//     while (1)
+//     {
+//         printf("Test Test\n");
+//         sleep(1);
+//     }
+
+
+
+//     return (0);
+// } 
