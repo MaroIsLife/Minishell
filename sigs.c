ced6598da841b045172ba80aa658264bf4e3ae5e
Ayoub Maatouch <contact@maatouch.com>
2021-03-09 19:26:16 +0100

    Fixed Ctrl+D and Ctrl+C

---
+++ sigs.c
@@ -0,0 +1,20 @@
+#include "minishell.h"
+
+void handler(int c)
+{
+
+	write(2,"\b\b  \b\b",5);
+	write(2,"\n",1);
+	print_prompt1();
+	//return 130
+
+}
+
+void handler2(int c)
+{
+
+	write(1,"Quit: ",5);
+	write(2,"\n",1);
+	exit (0);
+
+}
