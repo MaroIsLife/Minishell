cfb834453f0749b53745a4694e7d5937f2d6674b
Marouane Ougnou <mougnou@e1r12p4.1337.ma>
2021-03-09 19:31:45 +0100

    Added colors

--- misc.c
+++ misc.c
@@ -7,5 +7,5 @@ void clear()
 
 void    print_prompt1()
 {
-		write(1,"\033[0;32mMaro-$ \033[0;39m",sizeof("\033[0;32mMaro-$ \033[0;39m"));
+		write(1,"\033[0;32mMaro-&-Ma3toub$ \033[0;39m",sizeof("\033[0;32mMaro-&-Ma3toub$ \033[0;39m"));
 }
