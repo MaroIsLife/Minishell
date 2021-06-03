56e4903ebdf5ac4361ba5157fb6b726f038cc35f
Ayoub Maatouch <contact@maatouch.com>
2021-06-03 15:57:00 +0100

    new changer

--- testing.c
+++ testing.c
@@ -1,53 +1,54 @@
-// #include <unistd.h>
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+int main(int ac, char **av, char **env)
+{
+    char *cmd[2] = { "/bin/cat", NULL};
+    char *cmd2[2] = { "/bin/ls", NULL};
+    int fd[2];
 
+    // pipe(fd);
+    // if (fork() == 0)
+    // {
+    //     dup2(fd[1], 1);
+    //     close(fd[0]);
+    //     close(fd[1]);
+    //     execve("/bin/cat", cmd, env);
+    //     exit(0);
+    // }
+    // // wait(NULL);
+    // if (fork() == 0)
+    // {
+    //     dup2(fd[0], 0);
+    //     close (fd[1]);
+    //      close(fd[0]);
+    //     execve("/bin/ls", cmd2, env);
+    //     exit(0);
+    // }
+    // close(fd[0]);
+    // close(fd[1]);
+    // wait(NULL);
+    // wait(NULL);
 
-// int main(int argc, char **argv, char **envp)
-// {
-
-//     struct stat sb;
-
-//     printf("%d\n",stat("/Users/mougnou/Desktop/minishell/path.txt",&sb));
-//     // int fd[2];
-//     // // fd[0] - read end
-//     // // fd[1] - write end (Where we will write the data)
-
-//     // if (pipe(fd) == -1)
-//     //     printf("Error\n");
-
-//     // int id = fork();
-//     // if (id == 0) // Child
-//     // {
-//     //     close(fd[0]); //Closing fd 0 cause i dont use it to read on this process
-//     //     int x;
-//     //     x = 3;
-//     //     write(fd[1],&x,1); // Writing in Child proccess to Parent process
-//     //     close(fd[1]);
-//     // }
-//     // else // Parent 
-//     // {
-//     //     int y;
-//     //     close(fd[1]);
-//     //     read (fd[0],&y,sizeof(int));
-//     //     close(fd[0]);
-//     //     printf("We got %d\n",y);
-//     // }
-
-//     // char *av[] = { "AYOUB",argv[2], argv[3], argv[4]};
-//     // int o;
-//     // static int  i = 99;
-//     // if (fork () == 0)
-//     // {   printf ("-- return : %d\n", execve(ft_strjoin("/Users/$USER/.brew/bin/", argv[1]), av ,envp));
-//     //     //write(2, &i - 48 , 1);
-//     // }
-//     // else
-//     //     wait(&o); 
-//     // printf ("return : %d\n", i);
-//     // return 0;
-// }
+    	if (fork() == 0)
+		{	
+				dup2(fd[1], 1);
+				close(fd[0]);
+				// command_list(head->cmd, head->arg, head, src);
+				execve("/bin/cat", cmd, env);
+                exit(0);
+		}
+		close(fd[1]);
+		if (fork() == 0)
+		{
+			dup2(fd[0], 0);
+			close(fd[1]);
+             execve("/bin/ls", cmd2, env);
+			// command_list(head->pipe->cmd, head->pipe->arg, head, src);
+			exit(0);
+		}
+		close(fd[0]);
+		wait(NULL);
+		wait(NULL); 
+}
