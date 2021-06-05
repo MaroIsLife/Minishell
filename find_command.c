2d2ee2d4423b6e5a22789fff2c72f6190d635e2f
Maro <zidan-oug@hotmail.com>
2021-06-05 17:09:28 +0100

    Norminette fix

--- find_command.c
+++ find_command.c
@@ -39,12 +39,67 @@ int		get_env_value_cmd(char *s, char **envp, t_source *src, int i)
 	return (i);
 }
 
+int		find_command_three(char *s, t_source *src, t_node *head, int *i)
+{
+
+	if (s[(*i)] == '\'')
+		{
+			if (src->dquotes == 1)
+				src->ra[src->ra_b++] = s[(*i)];
+		}
+		else if (s[(*i)] == '\"')
+		{
+			if (src->squotes == 1)
+				src->ra[src->ra_b++] = s[(*i)];
+		}
+		else if (s[(*i)] == '\\' && ft_isascii(s[(*i) + 1]) == 1 && src->dquotes == 0 && src->squotes == 0)
+			src->ra[src->ra_b++] = s[++(*i)];
+		else if (s[(*i)] == '$' && src->squotes == 0 && (ft_isalpha(s[(*i) + 1]) == 1 || s[(*i) + 1] == '?') && src->aslash == 0)
+		{
+			(*i) = get_env_value_cmd(s, src->our_envp, src, (*i)) - 1;
+		}
+		else if ((s[(*i)] == '>' || s[(*i)] == '<') && src->aslash == 0 && src->dquotes == 0 && src->squotes == 0)
+		{
+			while (s[(*i)] == '>' || s[(*i)] == '<') //TEST THIS WITH ls>out1>out2 or >out1>out2 ls
+				find_file_name(&i, s, src, head);
+			(*i)++;
+			return 1 ;
+		}
+		else if (s[(*i)] == '\\' && src->aslash == 1)
+		{
+			if (s[(*i) + 1] != '\\')
+			{
+				src->ra[src->ra_b++] = s[(*i)++];
+				src->ra[src->ra_b++] = s[(*i)];
+			}				
+		}
+		else 
+			src->ra[src->ra_b++] = s[(*i)];
+		return (0);
+}
+
+int find_command_two(char *s, t_source *src, int i, t_node *head)
+{
+	int start;
+
+	start = i;
+	start = count_start(s, src, start, &i);
+	while (i < start)
+	{
+		finding_quotes(s, i, src);
+		if (find_command_three(s, src, head, &i) == 1)
+			continue ;
+		i++;
+	}
+	return (i);
+}
 
 char	*find_command(char *s, t_node *head, t_source *src, char **envp)
 {
 	int i;
 	int start;
 	int file_found;
+
 	file_found = 0;
 	i = src->offset;
 	// printf("bug: %s\n i: %d \n", s, i);
@@ -54,50 +109,12 @@ char	*find_command(char *s, t_node *head, t_source *src, char **envp)
 		i++;
 	start = i;
 	start = count_start(s, src, start, &i);
-
 	// printf("start: %d\n", start);
 	// src->ra = malloc((start + 1) * sizeof(char));
 	src->ra = malloc((4096) * sizeof(char));
-	while (i < start)
-	{
-		finding_quotes(s, i, src);
-		if (s[i] == '\'')
-		{
-			if (src->dquotes == 1)
-				src->ra[src->ra_b++] = s[i];
-		}
-		else if (s[i] == '\"')
-		{
-			if (src->squotes == 1)
-				src->ra[src->ra_b++] = s[i];
-		}
-		else if (s[i] == '\\' && ft_isascii(s[i + 1]) == 1 && src->dquotes == 0 && src->squotes == 0)
-			src->ra[src->ra_b++] = s[++i];
-		else if (s[i] == '$' && src->squotes == 0 && (ft_isalpha(s[i + 1]) == 1 || s[i + 1] == '?') && src->aslash == 0)
-		{
-			i = get_env_value_cmd(s, src->our_envp, src, i) - 1;
-		}
-		else if ((s[i] == '>' || s[i] == '<') && src->aslash == 0 && src->dquotes == 0 && src->squotes == 0)
-		{
-			while (s[i] == '>' || s[i] == '<') //TEST THIS WITH ls>out1>out2 or >out1>out2 ls
-				find_file_name(&i, s, src, head);
-			i++;
-			continue ;
-		}
-		else if (s[i] == '\\' && src->aslash == 1)
-		{
-			if (s[i + 1] != '\\')
-			{
-				src->ra[src->ra_b++] = s[i++];
-				src->ra[src->ra_b++] = s[i];
-			}				
-		}
-		else 
-			src->ra[src->ra_b++] = s[i];
-		i++;
-	}
+	i = find_command_two(s,src, i,head);
 	src->ra[src->ra_b] = '\0';
 	src->offset = i;
-	// printf("Offset: %d\n",src->offset);
+	printf("Offset: %d\n",src->offset);
 	return (src->ra);
 }
