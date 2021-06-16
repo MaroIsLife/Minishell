f6d00e32a67d3743a37efde5eac4de0e266fa1dc
Ayoub Maatouch <contact@maatouch.com>
2021-06-16 19:41:17 +0100

    PUshing

--- find_argument.c
+++ find_argument.c
@@ -1,5 +1,5 @@
 #include "minishell.h"
-int		arg_if_space(char *s,int ***i, t_source *src)
+int	arg_if_space(char *s, int ***i, t_source *src)
 {
 	if (s[***i] == ' ' && (src->dquotes == 1 || src->squotes == 1))
 		src->re[src->re_b++] = s[***i];
@@ -7,7 +7,8 @@ int		arg_if_space(char *s,int ***i, t_source *src)
 		(***i)++;
 	return (***i);
 }
-int check_offset (int offset, char *s)
+
+int	check_offset(int offset, char *s)
 {
 	int i = 0;
 	
@@ -17,62 +18,146 @@ int check_offset (int offset, char *s)
 		return (0);
 	return (offset);
 }
+
+int	count_argument_three(char *s, int **i, t_source *src)
+{
+	if (s[(**i)] == '\"' && (**i) == 0)
+		src->dquotes = 1;
+	else if (s[(**i)] == '\'' && (**i) == 0)
+		src->squotes = 1;
+	else
+		finding_quotes2(s, (**i), src);
+	if ((s[(**i)] == ';' || s[(**i)] == '|') && (src->dquotes == 0
+			&& src->squotes == 0 && src->aslash == 0) && s[(**i) - 1] != '\\')
+		return (1);
+	else
+		return (0);
+}
+
+int	count_argument_two(char *s, int *i, int *count, t_source *src)
+{
+	if (count_argument_three(s, &i, src) == 1)
+		return (1);
+	if (s[(*i)] == ' ' && src->dquotes == 0 && src->squotes == 0)
+	{
+		while (s[(*i)] == ' ')
+			(*i)++;
+		if ((s[(*i)] == '|' || s[(*i)] == ';') && src->dquotes == 0
+			&& src->squotes == 0 && src->aslash == 0 && s[(*i) - 1] != '\\')
+			return (1);
+		if (s[(*i)] == '\0' || s[(*i)] == '\n')
+			return (1);
+		if (s[(*i)] != '>' && s[(*i)] != '<' && src->tmp == 0)
+			(*count)++;
+		else
+			src->tmp = 0;
+	}
+	else if (s[(*i)] == '>' || s[(*i)] == '<')
+	{
+		(*count)++;
+		src->tmp = 1;
+		(*i)++;
+	}
+	else
+		(*i)++;
+	return (0);
+}
+
 int		count_argument(char *s, int offset, t_source *src) //CONVERT TO SPLIT?
 {
-	int i;
-	int jump;
-	int count;
-	// i = check_offset (src->offset - 1, s);
+	int	i;
+	int	jump;
+	int	count;
+
 	i = src->offset;
 	count = 0;
 	jump = 0;
 	if (s[0] == '\n')
-		return 0;
+		return (0);
 	if (i < 0)
-		return 0;
+		return (0);
 	if (s[i] == '|')
-		return 0;
+		return (0);
+	src->tmp = 0;
 	while (s[i] != '\0' && s[i] != '\n')
 	{
-		if (s[i] == '\"' && i == 0)
-			src->dquotes = 1;
-		else if (s[i] == '\'' && i == 0)
-			src->squotes = 1;
-		else
-			finding_quotes2(s, i, src);
-		if ((s[i] == ';' || s[i] == '|') && (src->dquotes == 0 && src->squotes == 0 && src->aslash == 0) && s[i - 1] != '\\')
-		{
+		if (count_argument_two(s, &i, &count, src) == 1)
 			return (count);
-		}
-		if (s[i] == ' ' && src->dquotes == 0 && src->squotes == 0)
-		{
-			while (s[i] == ' ')
-				i++;
-			if ((s[i] == '|' || s[i] == ';') && src->dquotes == 0 && src->squotes == 0 && src->aslash == 0 && s[i - 1] != '\\')
-				return (count);
-			if (s[i] == '\0' || s[i] == '\n')
-				return (count);
-			if (s[i] != '>' && s[i] != '<' && jump == 0) // Add != <
-				count++;
-			else
-				jump = 0;
-		}
-		else if (s[i] == '>' || s[i] == '<')
-		{
-			count++;
-			jump = 1;
-			i++;
-		}
-		else
-			i++;
 	}
 	return(count);
 }
 
+void	find_red_two(t_source *src, int option)
+{
+	if (option == 1)
+	{
+		if (src->fd_r_c == 2)
+			src->fd_r_c = 50;
+		else if (src->fd_r_c != 50)
+			src->fd_r_c = 1;
+	}
+	else if (option == 2)
+	{
+		if (src->fd_r_c == 1)
+			src->fd_r_c = 50;
+		else if (src->fd_r_c == 0)
+			src->fd_r_c = 2;
+	}
+}
+
+void	find_red(int ***i, char *s, t_source *src)
+{
+	if (s[***i] == '>' && s[***i + 1] == '>')
+	{
+		find_red_two(src, 1);
+		src->p->c = 94;
+		(***i) = (***i) + 2;
+	}
+	else if (s[***i] == '>')
+	{
+		find_red_two(src, 1);
+		(***i)++;
+		src->p->c = '>';
+	}
+	else if (s[***i] == '<')
+	{
+		find_red_two(src, 2);
+		(***i)++;
+		src->p->c = '<';
+	}
+}
+
+void	find_file_name_three(int ***i, char *s, t_source *src, t_filetmp *filetmp)
+{
+	if (s[***i] == '$' && src->squotes == 0)
+	{
+		filetmp->tmp = ft_strdup("");
+		(***i)++;
+		while (s[***i] != '\0' && s[***i] != '$')
+		{	
+			filetmp->tmp = ft_strdup(ft_strjoinchar(filetmp->tmp, s[***i]));
+			if (ft_search(src->our_envp, filetmp->tmp)
+				&& !ft_isalpha(s[(***i) + 1]))
+			{
+				filetmp->rev = get_x_env(src->our_envp, src, filetmp->tmp);
+				filetmp->j = 0;
+				while (filetmp->rev[filetmp->j] != '\0')
+				{	
+					src->p->filename[filetmp->b] = filetmp->rev[filetmp->j];
+					filetmp->j++;
+					filetmp->b++;
+				}
+			}
+			(***i)++;
+		}
+	}
+	else
+		src->p->filename[filetmp->b++] = s[(***i)++];
+}
 
 int		find_file_name(int **i, char *s, t_source *src, t_node *head)
 {
-	int			b;
+	t_filetmp	filetmp;
 
 	src->tmp2 = 1;
 	if (src->allocate == 1)
@@ -81,83 +166,19 @@ int		find_file_name(int **i, char *s, t_source *src, t_node *head)
 		src->p->next->next = NULL;
 		src->p = src->p->next;
 	}
-	if (s[**i] == '>' && s[**i + 1] == '>')
-	{
-		if (src->fd_r_c == 2) // src.fdrc == 2 if < comes first and fdrc == 50 if > comes first
-			src->fd_r_c = 50;
-		else if (src->fd_r_c != 50)
-			src->fd_r_c = 1;
-		src->p->c = 94;
-		(**i) = (**i) + 2;
-	}
-	else if (s[**i] == '>')
-	{
-		if (src->fd_r_c == 2)
-			src->fd_r_c = 50;
-		else if (src->fd_r_c != 50)
-			src->fd_r_c = 1;
-		(**i)++;
-		src->p->c = '>';
-	}
-	else if (s[**i] == '<')
-	{
-		if (src->fd_r_c == 1)
-			src->fd_r_c = 50;
-		else if (src->fd_r_c == 0)
-			src->fd_r_c = 2;
-		(**i)++;
-		src->p->c = '<';
-	}
-	b = 0;
+	find_red(&i, s, src);
 	while (s[**i] == ' ')
-		(**i)++;	
-	b = **i;
-	while (s[b] != '\0' && s[b] != '\n' && s[b] != '>' && s[b] != '<')
-	{
-		finding_quotes(s,b,src);
-		if (s[b] == ' ' && src->dquotes == 0 && src->squotes == 0)
-			break ;
-		else if (s[b] == '$' && src->squotes == 0)
-		{
-			;
-		}
-		// printf("c: %c----d: %d dquotes: %d squotes: %d\n",s[b], b,src->dquotes,src->squotes);
-		b++;
-	}
-	// src->p->filename = malloc((b + 1) * sizeof(char));
+		(**i)++;
 	src->p->filename = malloc((4096 + 1) * sizeof(char));
-	b = 0;
+	filetmp.b = 0;
 	while (s[**i] != '\0' && s[**i] != '\n' && s[**i] != '>' && s[**i] != '<')
 	{
-		finding_quotes(s,**i,src);
+		finding_quotes(s, **i, src);
 		if (s[**i] == ' ' && src->dquotes == 0 && src->squotes == 0)
 			break ;
-		if (s[**i] == '$' && src->squotes == 0)
-		{
-
-			char *tmp = ft_strdup("");
-			(*i)++;
-			while (s[**i] != '\0' && s[**i] != '$')
-			{	
-				 tmp = ft_strdup(ft_strjoinchar(tmp ,s[**i]));
-				if (ft_search(src->our_envp, tmp) && !ft_isalpha(s[(**i) + 1]))       
-				{
-					char* rev = get_x_env(src->our_envp, src, tmp);
-					int j = 0;
-					while (rev[j] != '\0')
-					{	
-						src->p->filename[b] = rev[j];
-						j++;
-						b++;
-					}
-				}
-				(*i)++;
-			}
-		}
-		else
-			src->p->filename[b++] = s[(**i)++];
+		find_file_name_three(&i, s, src, &filetmp);
 	}
-	src->p->filename[b] = '\0';
+	src->p->filename[filetmp.b] = '\0';
 	init_filee(src);
 	src->allocate = 1;
 	return (1);
@@ -165,12 +186,12 @@ int		find_file_name(int **i, char *s, t_source *src, t_node *head)
 
 void	find_argument_five(char *s, t_source *src, int ***i, int option)
 {
-	int o;
+	int	o;
 
 	o = (***i);
 	if (option == 1)
 	{
-		src->tmp = finding_aslash(s, o , src);
+		src->tmp = finding_aslash(s, o, src);
 		if (src->aslash == 0 && src->tmp != 1)
 		{
 			src->re[src->re_b++] = s[(***i)];
@@ -203,13 +224,13 @@ int		find_argument_four(char *s, t_source *src, int **i, int option)
 	}
 	else if (option == 4)
 	{
-		if(src->squotes == 1)	
+		if (src->squotes == 1)
 		{
 			src->re[src->re_b++] = s[(**i)];
 			return (0);
 		}
 		else
-			return (2); //break
+			return (2);
 	}
 		return (0);
 }
@@ -244,7 +265,7 @@ int		find_argument_three(char *s, t_source *src, int *i, t_node *head)
 
 int		find_argument_two(char *s, t_source *src, int i, t_node *head)
 {
-	int a;
+	int	a;
 
 	while (s[i] != '\n' && s[i] != '\0')
 	{
@@ -262,9 +283,8 @@ char	*find_argument(char *s, t_node *head, t_source *src, char **envp)
 {
 	int		i;
 	char	*re;
-	// i = check_offset (src->offset, s);
+
 	i = src->offset;
-	//  i = 0;
 	while (s[i] == ' ')
 		i++;
 	src->re = malloc((4096) * sizeof(char));
@@ -273,7 +293,8 @@ char	*find_argument(char *s, t_node *head, t_source *src, char **envp)
 	if (src->dquotes == 1 || src->squotes == 1 || src->aslash == 1)
 	{
 		printf("Error\n");
-		printf("Dquotes: %d, Squotes: %d, Aslash: %d\n",src->dquotes,src->squotes,src->aslash);
+		printf("Dquotes: %d, Squotes: %d, Aslash: %d\n", src->dquotes,
+			src->squotes, src->aslash);
 	}
 	src->offset = i;
 	src->re[src->re_b] = '\0';
