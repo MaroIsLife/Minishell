1583eb40dfd13768e6d7746f4b05f0f8d41abd55
Ayoub Maatouch <contact@maatouch.com>
2021-06-18 21:09:59 +0100

    here you go

--- red_open.c
+++ red_open.c
@@ -24,8 +24,6 @@ void	red_open_pipe_two(t_filename *p, int *fd, int *fd2)
 		dup2((*fd), 1);
 	else if (p->c == '<')
 		dup2((*fd2), 0);
-	close((*fd));
-	close((*fd2));
 }
 
 void	red_open_pipe(t_filename *tmp)
